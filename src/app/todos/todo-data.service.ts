45ec02fa53afe53a1cca69d47e3b7bae66cb411f
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-28 17:15:59 +0300

    add functin for post simulation

--- src/app/todos/todo-data.service.ts
+++ src/app/todos/todo-data.service.ts
@@ -7,14 +7,27 @@ export class TodoDataService {
   // placeholder for last id so we can simulate
   // automatic increamenting of id's
   lastId = 0;
+
   //placeholder for todo's
   todos: Todo[]=[];
+
+  constructor() { }
+
   //simulate POST/todos
-  addTodo(todo:Todo):todoDataService{
+  addTodo(todo:Todo):TodoDataService{
     if (!todo.id){
       todo.id = ++this.lastId;
     }
+    this.todos.push(todo);
+    return this;
   }
+  //simulate Delete/todos/:id
+  deleteTodoById(id:number):TodoDataService{
+    this.todos = this.todos
+    .filter(todo =>todo.id !==id);
+    return this;
+  }
+  
 
-  constructor() { }
+  
 }
