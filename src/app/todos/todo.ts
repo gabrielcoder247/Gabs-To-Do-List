a85d3507a0f02eacc79ac02f4e9b62195c84a42c
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-28 14:05:27 +0300

    create a class Todo

---
+++ src/app/todos/todo.ts
@@ -0,0 +1,12 @@
+export class Todo {
+    id: number;
+    title = '';
+    complete = false;
+    category:number;
+
+
+constructor(values:Object = {}){
+    Object.assign(this,values);
+
+    }
+}
