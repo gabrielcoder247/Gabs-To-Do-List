838a77ed37ff6962ce8b4ed04651cc0ab9aadb73
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-28 17:31:17 +0300

    add new categories

--- src/app/todos/todo-data.service.ts
+++ src/app/todos/todo-data.service.ts
@@ -4,30 +4,64 @@ import {Todo} from './todo';
   providedIn: 'root'
 })
 export class TodoDataService {
-  // placeholder for last id so we can simulate
-  // automatic increamenting of id's
+   // Placeholder for last id so we can simulate
+  // automatic incrementing of id's
   lastId = 0;
 
-  //placeholder for todo's
-  todos: Todo[]=[];
+    // Placeholder for todo's
+    todos: Todo[] = [];
 
   constructor() { }
 
-  //simulate POST/todos
-  addTodo(todo:Todo):TodoDataService{
-    if (!todo.id){
+  // Simulate POST /todos
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id) {
       todo.id = ++this.lastId;
     }
     this.todos.push(todo);
     return this;
   }
-  //simulate Delete/todos/:id
-  deleteTodoById(id:number):TodoDataService{
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoDataService {
     this.todos = this.todos
-    .filter(todo =>todo.id !==id);
+      .filter(todo => todo.id !== id);
     return this;
   }
-  
 
-  
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}):  Todo | any {
+    const todo = this.getTodoById(id) ;
+    if (!todo) {
+      return this;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number):  Todo | any {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Simulate GET /todos/:category
+  getTodoByCategory(id: number): Todo[] {
+    return this.todos
+      .filter(todo => todo.category === id);
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo) {
+    const updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
 }
